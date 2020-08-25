b058762e6fdd97702b2c4f2b67876666a7e52051
Dhairya <dhairya.chugh77@gmail.com>
2020-08-25 09:46:12 +0530

    Create Chef and Operators.cpp

---
+++ Chef and Operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t=0;
+	cin>>t;
+	int a=0,b=0;
+	for(int i=0;i<t;i++){
+	  cin>>a>>b;
+	  if(a==b) 
+	  cout<<"="<<endl;
+	  else if(a<b)
+	  cout<<"<"<<endl;
+	  else
+	  cout<<">"<<endl;
+	}
+	return 0;
+}
